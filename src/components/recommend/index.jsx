6b6adc1f6f68e750c63134632ddae2798f284f69
wwez <wwez@nne.com>
2020-06-27 17:01:31 +0800

    recommend

--- src/components/recommend/index.jsx
+++ src/components/recommend/index.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { getRecommend } from '../../api/recommend'
+import { ERR_OK } from '../../api/config'
 
 function Recommend() {
+	useEffect(() => {
+		getRecommend()
+			.then(res => {
+				if (res.code === ERR_OK) {
+					console.log(res.data)
+				}
+			})
+	})
   return (
     <div>Recommend</div>
   )
