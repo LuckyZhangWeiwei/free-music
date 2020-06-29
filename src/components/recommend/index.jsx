ddf1d80d8cea07974c24705a930cd934a1cb4eab
wwez <wwez@nne.com>
2020-06-29 11:46:03 +0800

    lazyload complete

--- src/components/recommend/index.jsx
+++ src/components/recommend/index.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState, useRef } from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 import { getRecommend, getDiscList } from '../../api/recommend'
 import { ERR_OK, ERR_OK_lOCAL } from '../../api/config'
 import Slider from '../../controls/slider'
 import Scroll from '../../controls/scroll'
 import './index.stylus'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 
 function Recommend() {
 	
@@ -45,7 +47,7 @@ function Recommend() {
 										return (
 											<div key={item.id}>
 												<a href={item.linkUrl}>
-													<img src={item.picUrl} alt="" className="needsclick" />
+													<img src={item.picUrl} alt=""  onLoad={loadImage} className="needsclick" />
 												</a>
 											</div>
 										)
@@ -63,7 +65,12 @@ function Recommend() {
 										return (
 											<li className="item" key={item.id}>
 													<div className="icon">
-														<img onLoad={loadImage} width="60" height="60" alt="" src={item.coverImgUrl} />
+														<LazyLoadImage
+															alt=""
+															height={60}
+															effect="blur"
+															src={item.coverImgUrl} // use normal <img> attributes as props
+															width={60} />
 													</div>
 													<div className="text">
 														<h2 className="name">{item.creator.nickname}</h2>
