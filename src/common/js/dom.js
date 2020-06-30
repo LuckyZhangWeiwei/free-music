e161c18ebaa3c4e0939a5aeca3b5c60340accaaf
wwez <wwez@nne.com>
2020-06-30 18:57:10 +0800

    shorcut start

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -12,3 +12,13 @@ export function addClass(el, className) {
   newClass.push(className)
   el.className = newClass.join(' ')
 }
+
+export function getData(el, name, value) {
+	const prefix = 'data-'
+	name = prefix + name
+	if (value) {
+		return el.setAttribute(name, value)
+	} else {
+		return el.getAttribute(name)
+	}
+}
--- src/controls/listview/index.jsx
+++ src/controls/listview/index.jsx
@@ -1,41 +1,69 @@
-import React from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import { LazyLoadImage } from 'react-lazy-load-image-component'
-import Scroll from '../scroll'
-
 import PropTypes from 'prop-types'
+import Scroll from '../scroll'
+import { getData } from '../../common/js/dom'
+
 
 import 'react-lazy-load-image-component/src/effects/blur.css'
 import './index.stylus'
 
 const ListView = function (props) {
 	const { data } = props
+	const scrollRef = useRef()
+	const [shortCutList, setShortCutList] = useState([]);
+
+	useEffect(() => {
+		const list = data.map(group => {
+			return group.title.substr(0, 1)
+		})
+		setShortCutList(list)
+	}, [data])
+
 	return (
-		<Scroll className="listview" {...props}>
-				<ul>
-				{
-					data.map((group, index) => {
-						return (
-							<li className="list-group" key={group.title}>
-								<h2 className="list-group-title">{group.title}</h2>
-								<ul>
-									{
-										group.items.map((item, index) => {
-											return (
-												<li className="list-group-item" key={item.name}>
-													<LazyLoadImage className="avatar" src={item.avatar} alt={item.name} effect="blur" />
-													<span className="name">{item.name}</span>
-												</li>
-											)
-										})
-									}
-								</ul>
-							</li>
+		<Scroll className="listview" ref={scrollRef} {...props}>
+			<ul>
+			{
+				data.map((group, index) => {
+					return (
+						<li className="list-group" key={group.title}>
+							<h2 className="list-group-title">{group.title}</h2>
+							<ul>
+								{
+									group.items.map((item, index) => {
+										return (
+											<li className="list-group-item" key={item.name}>
+												<LazyLoadImage className="avatar" src={item.avatar} alt={item.name} effect="blur" />
+												<span className="name">{item.name}</span>
+											</li>
+										)
+									})
+								}
+							</ul>
+						</li>
 						)
 					})
 				}
 			</ul>
+			<div className="list-shortcut" onTouchStart={onShortcutTouchStart}>
+				<ul>
+					{
+						shortCutList.map((item, index) => {
+							return (
+								<li className="item" key={item} data-index={index}>{item}</li>
+							)
+						})
+					}
+				</ul>
+			</div>
 		</Scroll>
 	)
+
+	function onShortcutTouchStart(e) {
+		let anchorIndex = getData(e.target, 'index')
+		console.log(anchorIndex)
+		console.log(scrollRef)
+	}
 }
 
 ListView.propTypes = {
