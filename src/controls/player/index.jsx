ebf64d1e03e9e8dcbdc7564b1493735a40892f61
wwez <wwez@nne.com>
2020-07-15 17:45:59 +0800

    7-4 complete

--- src/controls/player/index.jsx
+++ src/controls/player/index.jsx
@@ -1,20 +1,88 @@
-import React, {memo} from 'react'
+import React, {memo, useState, useEffect} from 'react'
 import { connect } from 'react-redux'
+
+import { setFullScreen } from '../../store/actions'
 
 import './index.stylus'
 
-const player = function(props) {
+const Player = function(props) {
+	const [currentSong, setCurrentSong] = useState()
+
+	useEffect(() => {
+		setCurrentSong(props.playList[props.currentIndex])
+	}, [props.playList, props.currentIndex])
+	
+	const close = function() {
+		props.dispatch(setFullScreen(false))
+	}
+
+	const open = function() {
+		props.dispatch(setFullScreen(true))
+	}
+	
+
 	return (
 		<div className="player">
 			{
-				props.isFullScreen 
+				props.isFullScreen && currentSong
 				&&
-				<div className="normal-player">播放器</div>
+				<div className="normal-player">
+					<div className="background">
+						<img width="100%" height="100%" src={currentSong.image} />
+					</div>
+					<div className="top">
+						<div className="back" onClick={() => close()}>
+							<i className="icon-back"></i>
+						</div>
+						<h1 className="title">{currentSong.name}</h1>
+						<h2 className="subtitle">{currentSong.singer}</h2>
+					</div>
+					<div className="middle">
+						<div className="middle-l">
+							<div className="cd-wrapper">
+								<div className="cd">
+									<img className="image" src={currentSong.image} />
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="bottom">
+						<div className="operators">
+							<div className="icon i-left">
+								<i className="icon-sequence"></i>
+							</div>
+							<div className="icon i-left">
+								<i className="icon-prev"></i>
+							</div>
+							<div className="icon i-center">
+								<i className="icon-play"></i>
+							</div>
+							<div className="icon i-right">
+								<i className="icon-next"></i>
+							</div>
+							<div className="icon i-right">
+								<i className="icon icon-not-favorite"></i>
+							</div>
+						</div>
+					</div>
+				</div>
 			}
 			{
-				!props.isFullScreen
+				!props.isFullScreen && currentSong
 				&&
-				<div className="mini-player"></div>
+				<div className="mini-player" onClick={() => open()}>
+					<div className="icon">
+						<img width="40" height="40" src={currentSong.image} />
+					</div>
+					<div className="text">
+						<h2 className="name">{currentSong.title}</h2>
+						<p className="desc">{currentSong.singer}</p>
+					</div>
+					<div className="control"></div>
+					<div className="control">
+						<i className="icon-playlist"></i>
+					</div>
+				</div>
 			}
 		</div>
 	)
@@ -26,4 +94,4 @@ export default connect(function mapStateToProps(state) {
 	return {
 		dispatch
 	}
-})(memo(player))
+})(memo(Player))
