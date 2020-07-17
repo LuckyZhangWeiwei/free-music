2aadf0a9652c35658716d75044945a895fc011c3
wwez <wwez@nne.com>
2020-07-17 10:54:09 +0800

    7-5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { BrowserRouter as Router } from "react-router-dom"
+import { connect } from 'react-redux'
 
 import Tab from './components/tab'
 import MHeader from './components/m-header'
 import Player from './controls/player'
 
-function App() {
-  return (
+function App(props) {
+	const [currentSong, setCurrentSong] = useState()
+ 
+	useEffect(() => {
+		setCurrentSong(props.playList[props.currentIndex])
+	}, [props.playList, props.currentIndex])
+  
+	return (
 		<>
 			<Router>
-					<MHeader />
-					<Tab />
+				<MHeader />
+				<Tab />
 			</Router>
-			<Player/>
+			{
+				currentSong 
+				&&
+				<Player currentSong={currentSong} />
+			}
 		</>
   )
 }
 
-export default App
+export default connect(function mapStateToProps(state) {
+	return state
+}, function mapDispatchToProps(dispatch) {
+	return {
+		dispatch
+	}
+})(App)
--- src/controls/player/index.jsx
+++ src/controls/player/index.jsx
@@ -3,98 +3,91 @@ import { CSSTransition } from 'react-transition-group'
 import { connect } from 'react-redux'
 
 import { setFullScreen } from '../../store/actions'
-import Loading from '../../controls/loading'
 
 import './index.stylus'
+import './index.css'
 
 const Player = function(props) {
-	const [currentSong, setCurrentSong] = useState()
- 
+	const [show, setShow] = useState(false)
+
 	useEffect(() => {
-		setCurrentSong(props.playList[props.currentIndex])
-	}, [props.playList, props.currentIndex])
+		setShow(true)
+	}, [])
+
+	useEffect(() => {
+		setShow(props.isFullScreen)
+	}, [props.isFullScreen])
 
 	const close = function() {
 		props.dispatch(setFullScreen(false))
+		setShow(false)
 	}
 
 	const open = function() {
 		props.dispatch(setFullScreen(true))
+		setShow(true)
 	}
 
 	return (
-		<>
-			{
-				currentSong ?
-				<div className="player">
-					{
-						props.isFullScreen ?
-							<CSSTransition timeout={4000} in={props.isFullScreen} classNames="normal">
-								<div className="normal-player">
-									<div className="background">
-										<img width="100%" height="100%" src={currentSong.image} />
-									</div>
-									<div className="top">
-										<div className="back" onClick={() => close()}>
-											<i className="icon-back"></i>
-										</div>
-										<h1 className="title">{currentSong.name}</h1>
-										<h2 className="subtitle">{currentSong.singer}</h2>
-									</div>
-									<div className="middle">
-										<div className="middle-l">
-											<div className="cd-wrapper">
-												<div className="cd">
-													<img className="image" src={currentSong.image} />
-												</div>
-											</div>
-										</div>
-									</div>
-									<div className="bottom">
-										<div className="operators">
-											<div className="icon i-left">
-												<i className="icon-sequence"></i>
-											</div>
-											<div className="icon i-left">
-												<i className="icon-prev"></i>
-											</div>
-											<div className="icon i-center">
-												<i className="icon-play"></i>
-											</div>
-											<div className="icon i-right">
-												<i className="icon-next"></i>
-											</div>
-											<div className="icon i-right">
-												<i className="icon icon-not-favorite"></i>
-											</div>
-										</div>
-									</div>
+		<div className="player">
+			<CSSTransition timeout={400} in={show} classNames="normal">
+				<div className="normal-player">
+					<div className="background">
+						<img width="100%" height="100%" src={props.currentSong.image} />
+					</div>
+					<div className="top">
+						<div className="back" onClick={() => close()}>
+							<i className="icon-back"></i>
+						</div>
+						<h1 className="title">{props.currentSong.name}</h1>
+						<h2 className="subtitle">{props.currentSong.singer}</h2>
+					</div>
+					<div className="middle">
+						<div className="middle-l">
+							<div className="cd-wrapper">
+								<div className="cd">
+									<img className="image" src={props.currentSong.image} />
 								</div>
-							</CSSTransition>
-							:
-							<CSSTransition timeout={4000} in={!props.isFullScreen}>
-								<div className="mini-player" onClick={() => open()}>
-									<div className="icon">
-										<img width="40" height="40" src={currentSong.image} />
-									</div>
-									<div className="text">
-										<h2 className="name">{currentSong.title}</h2>
-										<p className="desc">{currentSong.singer}</p>
-									</div>
-									<div className="control"></div>
-									<div className="control">
-										<i className="icon-playlist"></i>
-									</div>
-								</div>
-							</CSSTransition>
-					}
+							</div>
+						</div>
+					</div>
+					<div className="bottom">
+						<div className="operators">
+							<div className="icon i-left">
+								<i className="icon-sequence"></i>
+							</div>
+							<div className="icon i-left">
+								<i className="icon-prev"></i>
+							</div>
+							<div className="icon i-center">
+								<i className="icon-play"></i>
+							</div>
+							<div className="icon i-right">
+								<i className="icon-next"></i>
+							</div>
+							<div className="icon i-right">
+								<i className="icon icon-not-favorite"></i>
+							</div>
+						</div>
+					</div>
 				</div>
-				:
-				<div className="loading-container">
-						<Loading title="正在加载..." />
+			</CSSTransition>
+			<CSSTransition timeout={200} in={!show} classNames="mini">
+				<div className="mini-player" onClick={() => open()}>
+					<div className="icon">
+						<img width="40" height="40" src={props.currentSong.image} />
+					</div>
+					<div className="text">
+						<h2 className="name">{props.currentSong.title}</h2>
+						<p className="desc">{props.currentSong.singer}</p>
+					</div>
+					<div className="control"></div>
+					<div className="control">
+						<i className="icon-playlist"></i>
+					</div>
 				</div>
-			}
-		</>
+			</CSSTransition>
+		</div>
 	)
 }
 
