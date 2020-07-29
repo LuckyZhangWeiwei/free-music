06508a3725ab2cc39018195af5604fbd11f18fa1
wwez <wwez@nne.com>
2020-07-29 20:16:41 +0800

    commit

--- src/common/js/util.js
+++ src/common/js/util.js
@@ -1,11 +1,12 @@
 export function shuffle(arr) {
-	// for (let i = 0; i < arr.length; i++) {
-	// 	let j = getRandomInt(0, i)
-	// 	let t = arr[i]
-	// 	arr[i] = arr[j]
-	// 	arr[j] = t
-	// }
-	return arr.reverse()
+	for (let i = 0; i < arr.length; i++) {
+		let j = getRandomInt(0, i)
+		let t = arr[i]
+		arr[i] = arr[j]
+		arr[j] = t
+	}
+	return arr
+	// return arr.reverse()
 }
 
 function getRandomInt(min, max) {
