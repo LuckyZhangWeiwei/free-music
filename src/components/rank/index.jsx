ce5cb47ff4ee1aae649b38f252019adf289fcd4a
wwez <wwez@nne.com>
2020-08-11 15:13:17 +0800

    9-1 init

--- src/components/rank/index.jsx
+++ src/components/rank/index.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
+import './index.stylus'
+
 function Rank() {
   return (
-    <div>Rank</div>
+    <div className="rank">
+			<div className="toplist">
+				<ul>
+					<li className="item">
+						<div className="icon">
+							<img src="" alt="" width="100" height="100"/>
+						</div>
+						<ul className="songlist">
+							<li className="song">
+								<span></span>
+								<span></span>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
   )
 }
 
