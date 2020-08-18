b6a88d90d1f14df27134be1a6f7f91fa41214706
wwez <wwez@nne.com>
2020-08-18 10:14:38 +0800

    9-4 completed

--- src/api/rank.js
+++ src/api/rank.js
@@ -11,3 +11,19 @@ export function getTopList() {
 
 	return jsonp(url, data, options)
 }
+
+export function getMusicList(topid) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    topid,
+    needNewCode: 1,
+    uin: 0,
+    tpl: 3,
+    page: 'detail',
+    type: 'top',
+    platform: 'h5'
+  })
+
+  return jsonp(url, data, options)
+}
--- src/components/top-list/index.jsx
+++ src/components/top-list/index.jsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect, memo, useCallback } from 'react'
 import { CSSTransition } from 'react-transition-group'
 import { connect } from 'react-redux'
 import MusicList from '../../controls/music-list'
+import { getMusicList } from '../../api/rank'
+import { ERR_OK } from '../../api/config'
+import { creatSong } from '../../common/js/models/song'
 
 import './index.stylus'
 
@@ -11,6 +14,32 @@ const TopList = function (props) {
 
 	useEffect(() => {
 		setShow(true)
+	}, [])
+
+	useEffect(() => {
+		if (!props.topList.id) {
+				props.history.push({
+				pathname: '/rank'
+			})
+			return
+		}
+		getMusicList(props.topList.id).then(res => {
+			if (res.code === ERR_OK) {
+				setSong(_normalizeSongs(res.songlist))
+			}
+		})
+	}, [props.topList.id])
+
+	const _normalizeSongs = useCallback((songList) => {
+		const ret = []
+		songList.forEach(item => {
+			const musicData = item.data
+			
+			if (musicData.songid && musicData.albumid) {
+				ret.push(creatSong(musicData))
+			}
+		})
+		return ret
 	}, [])
 
 	return (
