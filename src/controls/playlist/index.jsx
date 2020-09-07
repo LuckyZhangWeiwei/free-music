33a0217d2ac968f085a96d44864c6f048bf45adc
wwez <wwez@nne.com>
2020-09-07 12:00:33 +0800

    11-4 completed

--- src/controls/playlist/index.jsx
+++ src/controls/playlist/index.jsx
@@ -1,21 +1,88 @@
 import React, {memo, useEffect, useState, useRef, useCallback} from 'react'
 import { CSSTransition } from 'react-transition-group'
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import Scroll from '../../controls/scroll'
+import { setCurrentIndex, setCurrentSong } from '../../store/actions'
+import { playMode } from '../../common/js/config'
 
 import './index.stylus'
+
+const MusicListItem = props => {
+
+	const getCurrentIcon = useCallback(item => {
+		const currentSong = props.currentSong
+		if (currentSong.id === item.id) {
+			return 'icon-play'
+		} else {
+			return ''
+		}
+	}, [props.currentIndex])
+
+	const selectItem = useCallback((e, item, index) => {
+		e.stopPropagation()
+		if (props.playMode === playMode.random) {
+			index = props.playList.findIndex(song => {
+				return song.id === item.id
+			})
+		}
+		props.dispatch(setCurrentIndex(index))
+		props.dispatch(setCurrentSong(props.playList[index]))
+	}, [])
+
+	return (
+			<li className="item" onClick={e => selectItem(e, props.item, props.index)}>
+				<i className={`current ${getCurrentIcon(props.item)}`}></i>
+				<span className="text">{props.item.name}</span>
+				<span className="like">
+					<i className="icon-not-favorite"></i>
+				</span>
+				<span className="delete">
+					<i className="icon-delete"></i>
+				</span>
+			</li>
+	)
+}
+
+const MusicListItemWithConnect = connect(
+	function mapStateToProps(state) {
+	return state
+}, function mapDispatchToProps(dispatch) {
+	return {
+		dispatch
+	}
+})(memo(MusicListItem))
+
 
 const PlayList = props => {
 	
 	const [show, setShow] = useState(false)
 
+	const scrollRef = useRef(null)
+
+	const listRef = useRef(null)
+
 	useEffect(() => {
 		setShow(true)
 	}, [])
+	
+	useEffect(() => {
+		scrollToCurrent()
+	}, [props.currentSong.id])
+
 
 	const hidePlaylist = useCallback(e => {
 		e.stopPropagation()
 		props.hidePlayList()
 	}, [])
+
+	const scrollToCurrent = () => {
+		const index = props.sequenceList.findIndex(song => {
+			return props.currentSong.id === song.id
+		})
+		scrollRef.current.scrollToElement(listRef.current.children[index], 300)
+	}
+
 	return (
 		<CSSTransition timeout={300} classNames="list-fade" in={show}>
 			<div className="playlist" onClick={e => hidePlaylist(e)}>
@@ -28,20 +95,23 @@ const PlayList = props => {
 							</i>
 						</h1>
 					</div>
-					<div className="list-content">
-						<ul>
-							<li className="item">
-								<i className="current"></i>
-								<span className="text"></span>
-								<span className="like">
-									<i className="icon-not-favorite"></i>
-								</span>
-								<span className="delete">
-									<i className="icon-delete"></i>
-								</span>
-							</li>
+					<Scroll className="list-content" data={props.sequenceList} ref={scrollRef}>
+						<ul ref={listRef}>
+							{
+								props.sequenceList.map((item, index) => {
+									return (
+										<MusicListItemWithConnect 
+											key={index} 
+											item={item} 
+											index={index}
+											scrollToCurrent = {() => scrollToCurrent()}
+										/>
+									)
+								})
+							}
+						
 						</ul>
-					</div>
+					</Scroll>
 					<div className="list-operate">
 						<div className="add">
 							<i className="icon-add"></i>
@@ -57,4 +127,11 @@ const PlayList = props => {
 	)
 }
 
-export default memo(PlayList)
+export default connect(
+	function mapStateToProps(state) {
+	return state
+}, function mapDispatchToProps(dispatch) {
+	return {
+		dispatch
+	}
+})(memo(PlayList))
