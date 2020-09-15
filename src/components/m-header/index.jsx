8f669ab59d3701ff04e576d4ecd53bbf86a67a51
wwez <wwez@nne.com>
2020-09-15 17:04:02 +0800

    change

--- src/components/m-header/index.jsx
+++ src/components/m-header/index.jsx
@@ -7,12 +7,13 @@ import logo from './timg.jpg'
 const MHeader = memo(function () {
   return (
     <div className="m-header">
-			<div className="header-icon" 
-			style={{
-				backgroundImage: `url(${logo})`,
-				position: 'absolute',
-				left: '20px'
-			}}/>
+			<div 
+				className="header-icon" 
+				style={{
+					backgroundImage: `url(${logo})`,
+					position: 'absolute',
+					left: '20px'
+				}}/>
       <h1 className="text">Free Music</h1>
 			<NavLink className="mine" to="/user">
 				<i className="icon-mine"></i>
