287dd09c7708bf40fca5fc5e1d3a0122d9402aa8
wwez <wwez@nne.com>
2020-09-15 15:39:27 +0800

    change

--- src/components/m-header/index.jsx
+++ src/components/m-header/index.jsx
@@ -2,14 +2,13 @@ import React, {memo} from 'react'
 import { NavLink } from "react-router-dom"
 
 import './index.styl'
-import logo from './logo@2x.png'
+import logo from './timg.jpg'
 
 const MHeader = memo(function () {
   return (
     <div className="m-header">
 			<div className="icon" style={{backgroundImage: `url(${logo})`}}/>
-			{/* <div className="icon"/> */}
-      <h1 className="text">Chicken Music</h1>
+      <h1 className="text">Free Music</h1>
 			<NavLink className="mine" to="/user">
 				<i className="icon-mine"></i>
 			</NavLink>
