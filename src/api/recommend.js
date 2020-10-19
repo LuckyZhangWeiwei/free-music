7ee9d525ff9344201837da6e00f10836aca6f0db
wwez <wwez@nne.com>
2020-10-19 21:38:28 +0800

    change

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,16 +1,6 @@
 import axios from 'axios'
-// import jsonp from '../common/js/jsonp'
-// import {commonParams, options} from './config'
 
 export function getRecommend() {
-  // const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
-  // const data = Object.assign({}, commonParams, {
-  //   platform: 'h5',
-  //   uin: 0,
-  //   needNewCode: 1
-  // })
-
-  // return jsonp(url, data, options)
 	const url = 'http://localhost:3000/dj/banner'
 	return axios.get(url, {
     params: null
